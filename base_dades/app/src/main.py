bbe914c98cf7ccb1a69bd9df88c1890f7b696b43
Adil <adil@nordkat.com>
2026-04-14 10:08:38 +0200

    Reestructured everything

--- base_dades/app/src/main.py
+++ base_dades/app/src/main.py
@@ -1,29 +1,16 @@
 import fastapi
-import sqlalchemy
-from .utils import db_engine
+import src.database.router as database
+
+
+
+
+
 app = fastapi.FastAPI()
 
 
-@app.get("/search")
+app.include_router(database.router)
+
+@app.get("/")
 def root():
     return {"message":"Hello, world!"}
 
-
-@app.get("/search/{item_id}")
-def getSCPSubject(item_id):
-
-    procedures = 0
-    description = 0
-    object_class = 0
-    data = None
-    with db_engine.connect() as db_con:
-
-        result = db_con.execute(sqlalchemy.text(f"SELECT * FROM scp_subjects WHERE id={item_id};"))
-        data = result.fetchone()
-
-    if data:
-        procedures = data[2]
-        description = data[3]
-        object_class = data[1]
-
-    return {"Item-#":item_id, "object_class":object_class ,"procedures": procedures, "description":description}
--- base_dades/app/src/utils/__init__.py
+++ base_dades/app/src/utils/__init__.py
@@ -1,3 +1,7 @@
 from sqlalchemy import create_engine
 
 db_engine = create_engine("mysql+pymysql://admin:SCP!PROJECT@db/scp")
+
+def updateDatabase():
+    from os import system
+    system("poetry run alembic upgrade head")
