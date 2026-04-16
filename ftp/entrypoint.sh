99dffad30227bb967a0124f0884039fba0d65d5a
Adil <adil@nordkat.com>
2026-04-16 11:14:46 +0200

    FTP:Added volume to save files

--- ftp/entrypoint.sh
+++ ftp/entrypoint.sh
@@ -1,3 +1,7 @@
 service ssh start
 service vsftpd start
+chown root:root /ftp -R
+mkdir -p ftp ftp/files/scp_files ftp/files/incidents_files ftp/files/other_files
+#chmod -R 777 /ftp
+#chmod 755 /ftp
 tail -f /dev/null
